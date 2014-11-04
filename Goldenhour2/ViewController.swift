f6daf4f4f68aa39ad1cb2ebe6badfe6ad23647d0
Alex Meckes <alex.meckes@gmail.com>
2014-11-03 20:34:45 -0500

    Golden Hour 2.0
    initial commit

--- Goldenhour2/ViewController.swift
+++ Goldenhour2/ViewController.swift
@@ -11,6 +11,19 @@ import UIKit
 class ViewController: UIViewController {
                             
     override func viewDidLoad() {
+        let date:NSDate = NSDate()
+        let sunCalc:SunCalc = SunCalc.getTimes(date, latitude: 51.5, longitude: -0.1)
+        
+        var formatter:NSDateFormatter = NSDateFormatter()
+        formatter.dateFormat = "HH:mm"
+        formatter.timeZone = NSTimeZone(abbreviation: "GMT")
+        var sunriseString:String = formatter.stringFromDate(sunCalc.sunrise)
+        println("sunrise is at \(sunriseString)")
+        
+        let sunPos:SunPosition = SunCalc.getSunPosition(date, latitude: 51.5, longitude: -0.1)
+        
+        var sunriseAzimuth:Double = sunPos.azimuth * 180 / Constants.PI()
+        println("sunrise azimuth: \(sunriseAzimuth)")
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
@@ -22,4 +35,3 @@ class ViewController: UIViewController {
 
 
 }
-
