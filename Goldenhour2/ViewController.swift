c79e66b22d3fed97cdd1bc685b3a3d296760cbf7
Alex Meckes <alex.meckes@gmail.com>
2014-11-03 20:14:32 -0500

    Initial Commit

---
+++ Goldenhour2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Goldenhour2
+//
+//  Created by Alex Meckes on 11/3/14.
+//  Copyright (c) 2014 Alex Meckes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
