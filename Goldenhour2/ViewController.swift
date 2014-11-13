01da6d91ad61574ab255d6fe6d630aa66d277176
Alex Meckes <alex.meckes@gmail.com>
2014-11-12 20:28:34 -0500

    location
    adding lat and long

--- Goldenhour2/ViewController.swift
+++ Goldenhour2/ViewController.swift
@@ -7,31 +7,56 @@
 //
 
 import UIKit
+import CoreLocation
 
-class ViewController: UIViewController {
-                            
+
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    
+    @IBOutlet weak var locLabel: UILabel!
+    @IBOutlet weak var lonLabel: UILabel!
+    @IBOutlet weak var latLabel: UILabel!
+    @IBOutlet weak var MyLocation: UITextField!
+    var locationManager: CLLocationManager!
+    
     override func viewDidLoad() {
-        let date:NSDate = NSDate()
-        let sunCalc:SunCalc = SunCalc.getTimes(date, latitude: 51.5, longitude: -0.1)
-        
-        var formatter:NSDateFormatter = NSDateFormatter()
-        formatter.dateFormat = "HH:mm"
-        formatter.timeZone = NSTimeZone(abbreviation: "GMT")
-        var sunriseString:String = formatter.stringFromDate(sunCalc.sunrise)
-        println("sunrise is at \(sunriseString)")
-        
-        let sunPos:SunPosition = SunCalc.getSunPosition(date, latitude: 51.5, longitude: -0.1)
-        
-        var sunriseAzimuth:Double = sunPos.azimuth * 180 / Constants.PI()
-        println("sunrise azimuth: \(sunriseAzimuth)")
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.locationManager = CLLocationManager()
     }
-
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func locButton(sender: AnyObject) {
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestAlwaysAuthorization()
+        locationManager.startUpdatingLocation()
     }
 
+    //func locationManager(manager:CLLocationManager!, didUpdateLocations locations:[AnyObject]!) {
+      //  println("locations = \(locationManager)")
+       // var latValue = locationManager.location.coordinate.latitude
+        //var lonValue = locationManager.location.coordinate.longitude
+        //latLabel.text = String(latValue)
+        //lonLabel.text = String(lonValue)
+     //   locLabel.text = "success"
+   // }
 
 }
+
+
+   // let date:NSDate = NSDate()
+   // let sunCalc:SunCalc = SunCalc.getTimes(date, latitude: 51.5, longitude: -0.1)
+    
+    //var formatter:NSDateFormatter = NSDateFormatter()
+    //formatter.dateFormat = "HH:mm"
+    //formatter.timeZone = NSTimeZone(abbreviation: "GMT")
+   // var sunriseString:String = formatter.stringFromDate(sunCalc.sunrise)
+    //println("sunrise is at \(sunriseString)")
+    
+   // let sunPos:SunPosition = SunCalc.getSunPosition(date, latitude: 51.5, longitude: -0.1)
+    
+   // var sunriseAzimuth:Double = sunPos.azimuth * 180 / Constants.PI()
+   // println("sunrise azimuth: \(sunriseAzimuth)")
+   // super.viewDidLoad()
+// Do any additional setup after loading the view, typically from a nib.
